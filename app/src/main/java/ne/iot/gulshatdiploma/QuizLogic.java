2419e060f2922d594706a09725851dd57210e525
Didar <gartaganow7490@gmail.com>
2021-12-07 18:55:39 +0500

    The third commit

--- app/src/main/java/ne/iot/gulshatdiploma/QuizLogic.java
+++ app/src/main/java/ne/iot/gulshatdiploma/QuizLogic.java
@@ -1,7 +1,5 @@
 package ne.iot.gulshatdiploma;
 
-import java.util.Random;
-
 public class QuizLogic {
     String equation;
     long solution;
@@ -11,18 +9,13 @@ public class QuizLogic {
         long num1 = Math.round(Math.random() * 100);
         long num2 = Math.round(Math.random() * 100);
 
-        final int random = new Random().nextInt(11) + 10; // [0, 60] + 20 => [20, 80]
-        long num3 = random;
-        long num4 = random;
-
-        String[] operators = {"+", "*"};
+        String[] operators = {"+", "-"};
         String operator = operators[ (int) Math.round(Math.random())];
 
         if (operator.equals("+")) {
             solution = num1 + num2;
-        }
-        else {
-            solution = num3 * num4;
+        } else {
+            solution = num1 - num2;
         }
 
         equation = String.format("%s %s %s", num1, operator, num2);
